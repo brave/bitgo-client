e7b892db9f1709a6910821c7c623b9bc33d89c98
yan <yan@brave.com>
2020-02-28 17:19:56 -0800

    Fill out signTransaction and return bitgoPub in create-wallet

--- util.js
+++ util.js
@@ -23,20 +23,56 @@ module.exports = {
       // BitGo wallet creation errors if the user and backup keys are the same.
       seed[0]++
     }
-    const keychain = bitgo.coin(coin).keychains().create(seed)
-    return keychain
+    return bitgo.coin(coin).keychains().create(seed)
   },
 
   /**
    * Signs a transaction with the local key.
-   * TODO: waiting on BitGO to split up the SDK to make this work.
-   * @param {Object} wallet - wallet instance
-   * @param {Object} prebuildTx - prebuilt transaction
+   * @param {string} coin - type of the wallet; ex: 'btc'
+   * @param {ArrayBuffer} seed - 32-byte input seed
+   * @param {string} bitgoPub - bitgo public key
+   * @param {Object} txPrebuild - transaction prebuild object from bitgo
+   * @param {string} address - destination address
+   * @param <number> amount - amount in base value (satoshi/wei/etc.)
    */
-  signTransaction: async (wallet, prebuildTx) => {
-    const halfSigned = await wallet.prebuildAndSignTransaction({
-      prebuildTx
-    })
-    return halfSigned
+  signTransaction: async (coin, seed, bitgoPub, txPrebuild, address, amount) => {
+    // create a dummy wallet object
+    const wallet = bitgo.coin(coin).newWalletObject({})
+    // create transaction params from the user input
+    const txParams = {
+      recipients: [{
+        address,
+        amount
+      }]
+    }
+    // all 3 keychains are needed for verifyTransaction
+    const userKeychain = module.exports.createKeychain(coin, seed)
+    const backupKeychain = module.exports.createKeychain(coin, seed, true)
+
+    // verify transaction received from brave actually sends the right
+    // amount to the address we are expecting
+    const verifyOptions = {
+      txParams,
+      txPrebuild,
+      wallet,
+      verification: {
+        keychains: [
+          { pub: userKeychain.pub },
+          { pub: backupKeychain.pub },
+          { pub: bitgoPub }],
+        disableNetworking: true
+      }
+    }
+    // verify transaction will throw if the transaction cannot be verified
+    await wallet.baseCoin.verifyTransaction(verifyOptions)
+
+    // sign the transaction, providing the tx prebuild, the encrypted user keychain, and the wallet passphrase.
+    const signOptions = {
+      txPrebuild,
+      keychain: {
+        prv: userKeychain.prv
+      }
+    }
+    return wallet.signTransaction(signOptions)
   }
 }
